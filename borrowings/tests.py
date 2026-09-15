1aff29a995df406aa952b50691e636478786d048
Vlad <vlad@exampl.com>
2026-09-15 15:57:33 +0300

    Test borrowing visibility and filters

--- borrowings/tests.py
+++ borrowings/tests.py
@@ -9,7 +9,7 @@ from books.models import Book
 from borrowings.models import Borrowing
 
 
-class BorrowingVisibilityTests(APITestCase):
+class BorrowingSetUp(APITestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
             email="a@gmail.com", password="a"
@@ -24,19 +24,49 @@ class BorrowingVisibilityTests(APITestCase):
             inventory=5,
             daily_fee="10.20",
         )
-
-    def test_books_visible_for_admin(self):
-        expected_return_date = date.today() + timedelta(days=7)
-        borrowing1 = Borrowing.objects.create(
-            expected_return_date=expected_return_date,
+        self.expected_return_date = date.today() + timedelta(days=7)
+        self.borrowing1 = Borrowing.objects.create(
+            expected_return_date=self.expected_return_date,
             book=self.book,
             user=self.user
         )
-        borrowing2 = Borrowing.objects.create(
-            expected_return_date=expected_return_date,
+        self.borrowing2 = Borrowing.objects.create(
+            expected_return_date=self.expected_return_date,
             book=self.book,
             user=self.admin
         )
+
+
+class BorrowingVisibilityTests(BorrowingSetUp):
+    def test_admin_sees_all_borrowings(self):
         self.client.force_authenticate(self.admin)
         res = self.client.get(reverse("borrowings:borrowing-list"))
-        self.assertEqual({item["id"] for item in res.data}, {borrowing1.id, borrowing2.id})
+        self.assertEqual({item["id"] for item in res.data}, {self.borrowing1.id, self.borrowing2.id})
+
+    def test_user_sees_only_own_borrowings(self):
+        self.client.force_authenticate(self.user)
+        res = self.client.get(reverse("borrowings:borrowing-list"))
+        self.assertEqual({item["id"] for item in res.data}, {self.borrowing1.id})
+
+
+class BorrowingFilterTests(BorrowingSetUp):
+    def test_filter_isactive(self):
+        Borrowing.objects.create(
+            expected_return_date=self.expected_return_date,
+            actual_return_date=self.expected_return_date,
+            book=self.book,
+            user=self.user
+        )
+        self.client.force_authenticate(self.user)
+        res = self.client.get(reverse("borrowings:borrowing-list"), {"is_active": "true"})
+        self.assertEqual({item["id"] for item in res.data}, {self.borrowing1.id})
+    
+    def test_filter_user_id_for_admin(self):
+        self.client.force_authenticate(self.admin)
+        res = self.client.get(reverse("borrowings:borrowing-list"), {"user_id": self.user.id})
+        self.assertEqual({item["id"] for item in res.data}, {self.borrowing1.id})
+
+    def test_filter_user_id_for_user(self):
+        self.client.force_authenticate(self.user)
+        res = self.client.get(reverse("borrowings:borrowing-list"), {"user_id": self.admin.id})
+        self.assertEqual({item["id"] for item in res.data}, {self.borrowing1.id})
